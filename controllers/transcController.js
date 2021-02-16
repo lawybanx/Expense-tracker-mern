ab783d946080f2e8ffaabdd13f62521c345e7dfb
Lawybanx <awwalbankole@gmail.com>
2021-02-16 04:46:56 +0100

    refactor file structure to client and backend

---
+++ controllers/transcController.js
@@ -0,0 +1,79 @@
+// Bring in Model
+const Transaction = require('../models/Transaction');
+
+//  @route  GET api/transactions
+//  @desc   Get All Transactions
+//  @access Public
+exports.getTransactions = async (req, res, next) => {
+  try {
+    const transactions = await Transaction.find();
+
+    return res.status(200).json({
+      success: true,
+      count: transactions.length,
+      data: transactions,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      error: 'Server Error',
+    });
+  }
+};
+
+//  @route  POST api/transactions
+//  @desc   Create A Transaction
+//  @access Public
+exports.addTransaction = async (req, res, next) => {
+  const { text, amount } = req.body;
+  try {
+    const transaction = await Transaction.create(req.body);
+
+    return res.status(201).json({
+      success: true,
+      data: transaction,
+    });
+  } catch (err) {
+    if (err.name === 'ValidationError') {
+      const messages = Object.values(err.errors).map((val) => val.message);
+
+      return res.status(400).json({
+        success: false,
+        error: messages,
+      });
+    } else {
+      return res.status(500).json({
+        success: false,
+        error: 'Server Error',
+      });
+    }
+  }
+};
+
+//  @route  DELETE api/transactions/:id
+//  @desc   Delete A Transaction
+//  @access Public
+exports.deleteTransaction = async (req, res, next) => {
+  try {
+    const transaction = await Transaction.findById(req.params.id);
+
+    if (!transaction) {
+      return res.status(404).json({
+        success: false,
+        error: 'No Transasction Found',
+      });
+    }
+
+    await transaction.remove();
+
+    return res.status(200).json({
+      success: true,
+      data: {},
+    });
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      error: 'Server Error',
+    });
+  }
+};
