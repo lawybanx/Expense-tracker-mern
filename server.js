f0ba8e6a67e8b9900d804580edf2e8abea358da5
Lawybanx <awwalbankole@gmail.com>
2021-02-16 07:32:21 +0100

    Add ststic production build folder

--- server.js
+++ server.js
@@ -11,15 +11,21 @@ connectDB();
 // Express Init
 const app = express();
 
-// Static Folder
-// app.use(express.static(path.join(__dirname, 'public')));
-
 // Body-parser Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Routes
 app.use('/api/transactions', require('./routes/api/transactions'));
+
+if (process.env.NODE_ENV === 'production') {
+  // Static Folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 // Port
 const PORT = process.env.PORT || 5000;
