8799f09666d50669a8c062385e1e3df48142308a
Lawybanx <awwalbankole@gmail.com>
2021-02-16 07:34:23 +0100

    connect backend to client

--- client/src/components/Transactions.js
+++ client/src/components/Transactions.js
@@ -1,16 +1,21 @@
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import { GlobalContext } from '../context/GlobalState';
 import Transaction from './Transaction';
 
 const Transactions = () => {
-  const { transactions } = useContext(GlobalContext);
+  const { transactions, getTransactions } = useContext(GlobalContext);
+
+  useEffect(() => {
+    getTransactions();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <>
       <h3>ヒストリー</h3>
       <ul className="list">
         {transactions.map((transaction) => (
-          <Transaction key={transaction.id} transaction={transaction} />
+          <Transaction key={transaction._id} transaction={transaction} />
         ))}
       </ul>
     </>
